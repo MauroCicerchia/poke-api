b5bb0f6d2cce3a464113a5abb3ab6deb04641de8
MauroCicerchia <maurocicerchia98@gmail.com>
2021-01-29 23:56:05 -0300

    feat: add cors

--- src/init/express.js
+++ src/init/express.js
@@ -1,7 +1,9 @@
+import cors from "cors";
 import morgan from "morgan";
 import apiRouter from "../api/routes";
 
 export default app => {
+    app.use(cors());
     app.use(morgan("dev"));
     app.use("/api", apiRouter);
 };
